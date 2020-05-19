15e5e3bb395119406544a941c5e6d525d2b04364
jackfriend <jackwillamfriend@gmail.com>
2020-05-19 19:48:35 -0400

    integrated config

--- panda.py
+++ panda.py
@@ -1,6 +1,7 @@
 from panda_extension import *
+from config import input_path, output_path
 
-df = init_data("aim_one.csv")
+df = init_data(input_path)
 
 # comment out specific y axes not wanted
 y_axes = [
@@ -39,7 +40,7 @@ caption = """
 make_plot(df=df, x="dias_en_foam", y=y_axes, plotting_method=make_boxsubplot_by_trait, extra_info=axis_info, caption=caption)
 # plt.tight_layout()
 plt.draw()
-plt.savefig("figs.jpg",
+plt.savefig(output_path,
         dpi=None,
         quaility=None, # JPG quality; 1 <= x <= 95; None defaults to 95
         optimize=True, # optimizes JPEGs
--- panda_extension.py
+++ panda_extension.py
@@ -28,7 +28,7 @@ def init_data(file_name=False, data_type="csv", style="white"):
 
     # default case
     if data_type == "csv":
-        df = pd.read_csv('aim_one.csv', na_values=['#VALUE!', '#DIV/0!'])
+        df = pd.read_csv(file_name, na_values=['#VALUE!', '#DIV/0!'])
         return df
     #TODO: Add cases for non-csv file types
     else:
