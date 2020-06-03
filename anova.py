f92be6c0d40a20f0fec48896fec52ada3beed14e
jackfriend <jackwillamfriend@gmail.com>
2020-06-02 23:25:05 -0400

    increased modularity

--- anova.py
+++ anova.py
@@ -4,49 +4,51 @@ import scipy.stats as stats
 from panda_extension import *
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
-from statsmodel.stats.multicomp import pairwise_turkeyhsd
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 from panda_extension import *
 import config
 
 
-def clean_up(df, log=False):
+def clean_up(df, x=None, y=None, log=False):
     # clean up
-    df = df[['total_length', 'dias_en_foam']]
-    df = df.set_index(['dias_en_foam', df.groupby('dias_en_foam').cumcount()])['total_length'].unstack(0)
-    # df = df.drop([2])
+    df = df[[x, y]]
+    df = df.set_index([x, df.groupby(x).cumcount()])[y].unstack(0)
 
     if log:
-        df[0] = numpy.log(df[0])
-        df[2] = numpy.log(df[2])
-        df[4] = numpy.log(df[4])
-        df[6] = numpy.log(df[6])
+        for col in df:
+            df[col] = numpy.log(df[col])
 
     return df
 
 
-def fstat_and_pvalue(df, *args):
+def fstat_and_pvalue(*args):
     # let's calculate the F stat and P value. Drop N/A values, or f_oneway won't work
-    fstat, pvalue = stats.f_oneway(args[0].dropna(), args[1].dropna(), args[2].dropna())
+    mut_args = [] # *args gives us a tuple, we need to make a list
+    for arg in args:
+        mut_args.append(arg.dropna())
+
+    fstat, pvalue = stats.f_oneway(*mut_args) # the * unpacks the list into seperate variables
     return fstat, pvalue
 
 
-def get_anova(dataframe):
+def get_anova(dataframe, x=None, y=None, categories=[]):
+    # get_anova(df, x="dias_en_foam", y="total_length", categories=[0, 2, 4, 6])
     melt = pd.melt(dataframe.reset_index(),
             id_vars=['index'], # id_vars=['index'] mantains value pairings
-            value_vars=[0, 2, 4, 6])
-    melt.columns = ['index', 'dias_en_foam', 'total_length']
+            value_vars=categories)
+    melt.columns = ['index', x, y]
 
-    model = ols("total_length ~ dias_en_foam", data=melt).fit()
+    model = ols("{0} ~ {1}".format(y, x), data=melt).fit()
     anova_table = sm.stats.anova_lm(model, typ=2)
     return anova_table
 
 
 if __name__ == "__main__":
     df = init_data(config.input_path)
-    df = clean_up(df)
-    fstat, pvalue = fstat_and_pvalue(df, df[2], df[4], df[6])
-    anova = get_anova(df)
+    df = clean_up(df, x="dias_en_foam", y="total_length")
+    fstat, pvalue = fstat_and_pvalue(df[2], df[4], df[6])
+    anova = get_anova(df, x="dias_en_foam", y="total_length", categories=[0, 2, 4, 6])
 
     print(df)
     print("F statistic: {0}\nP value: {1}".format(fstat, pvalue))
@@ -56,9 +58,9 @@ if __name__ == "__main__":
     df = None # reset df
 
     df = init_data(config.input_path)
-    df = clean_up(df, log=True)
-    fstat, pvalue = fstat_and_pvalue(df, df[2], df[4], df[6])
-    anova = get_anova(df)
+    df = clean_up(df, x="dias_en_foam", y="total_length", log=True)
+    fstat, pvalue = fstat_and_pvalue(df[2], df[4], df[6])
+    anova = get_anova(df, x="dias_en_foam", y="total_length", categories=[0, 2, 4, 6])
 
     print(df)
     print("F statistic: {0}\nP value: {1}".format(fstat, pvalue))
