9072389a5df2894e271ed3b0ad863ace66fe843e
jackfriend <jackwillamfriend@gmail.com>
2020-07-10 14:20:20 -0400

    pca comp function

--- r/pca.r
+++ r/pca.r
@@ -44,41 +44,70 @@ ggplot(data, aes(PC1, PC2, col=dias_en_foam, fill=dias_en_foam)) +
    geom_point(shape=21, col="black")
 
 #ANOVA of PCA
-anova_data <- data %>%
-    select(clutch, dias_en_foam, PC4) %>% # Change the PC to the PC we want to use
-    group_by(dias_en_foam)
+get_anova_of_pca <- function(dataset, PC_comp, PC_comp_str) {
 
-summary <- anova_data %>%
-    summarize(PC4.mean = mean(PC4, na.rm=TRUE),
-              PC4.sd = sd(PC4, na.rm=TRUE),
-              PC4.N = length(PC4),
-              PC4.se = PC4.sd/sqrt(PC4.N),
-              PC4.95_lower = ci(PC4, na.rm=TRUE)[2],
-              PC4.95_upper = ci(PC4, na.rm=TRUE)[3],
-              PC4.min = min(PC4, na.rm=TRUE),
-              PC4.max = max(PC4, na.rm=TRUE))
+    anova_data <- dataset %>%
+        select(clutch, dias_en_foam, {{PC_comp}}) %>% # Change the PC to the PC we want to use
+        group_by(dias_en_foam)
 
+    summary <- anova_data %>%
+        summarize(
+                  N        := length( {{PC_comp}}),
+                  mean     := mean( {{PC_comp}}, na.rm=TRUE),
+                  sd       := sd( {{PC_comp}},  na.rm=TRUE),
+                  se       := ( sd / sqrt( N )),
+                  lower    := ci( {{PC_comp}} , na.rm=TRUE)[2],
+                  upper    := ci( {{PC_comp}} , na.rm=TRUE)[3],
+                  min      := min( {{PC_comp}} , na.rm=TRUE),
+                  max      := max( {{PC_comp}} , na.rm=TRUE)
+                  )
+
+    print("___________________________________________________________________________")
+    print("SUMMARY:")
+    print(summary)
+    print(anova_data)
+
+    # linear model
+    lm_formula <- paste(PC_comp_str, "~ dias_en_foam")
+    linear_model <- lm(lm_formula, data=anova_data)
+    # Log linear model
+    log_lm_formula <- paste("log(", PC_comp_str, ") ~ dias_en_foam")
+    log_linear_model <- lm(log_lm_formula, data=anova_data)
+    # ANOVA
+    anova_lm <- anova(linear_model)
+    log_anova_lm <- anova(log_linear_model)
+
+    print("___________________________________________________________________________")
+    print("ANOVA MODEL:")
+    print(summary(linear_model))
+    print(anova_lm)
+
+    print("___________________________________________________________________________")
+    print("LOG ANOVA MODEL:")
+    print(summary(log_linear_model))
+    print(log_anova_lm)
+}
+
+print("")
+print("")
 print("___________________________________________________________________________")
-print("SUMMARY:")
-print(summary)
-
-linear_model <- lm(PC4 ~ dias_en_foam, data=anova_data)
-log_linear_model <- lm(log(PC4) ~ dias_en_foam, data=anova_data)
-anova_lm <- anova(linear_model)
-log_anova_lm <- anova(log_linear_model)
-
+print("PC1:")
+get_anova_of_pca(data, PC1, "PC1")
+print("")
+print("")
 print("___________________________________________________________________________")
-print("ANOVA MODEL:")
-print(summary(linear_model))
-print(anova_lm)
-
+print("PC2:")
+get_anova_of_pca(data, PC2, "PC2")
+print("")
+print("")
 print("___________________________________________________________________________")
-print("LOG ANOVA MODEL:")
-print(summary(log_linear_model))
-print(log_anova_lm)
-
-
-
+print("PC3:")
+get_anova_of_pca(data, PC3, "PC3")
+print("___________________________________________________________________________")
+print("")
+print("")
+print("PC4:")
+get_anova_of_pca(data, PC4, "PC4")
 
 # Output data to file
 dev.off()
